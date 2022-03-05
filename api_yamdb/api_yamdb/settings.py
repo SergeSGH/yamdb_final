334aa6fab9c7832eb0fdecb998f79ca7131fcbf1
Sergey <ssprihodko@yandex.ru>
2022-03-05 14:14:58 +0300

    collectstatic

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,7 +116,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'docs/'),
+    )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
